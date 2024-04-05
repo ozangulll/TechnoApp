d3ec425ddea007d28ff82e23a04f480e02abc1c5
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-04-05 20:55:55 +0300

    create Category entities

---
+++ ShopApp.Entities/Category.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<ProductCategory> ProductCategories { get; set; }
+    }
+}
