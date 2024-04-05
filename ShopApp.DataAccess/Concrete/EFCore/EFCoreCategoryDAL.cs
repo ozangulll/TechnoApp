92bca1853bfa0d38d7885155b1d975c47922972b
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-04-05 21:12:04 +0300

    Category and product dal classes added inherited interfaces

---
+++ ShopApp.DataAccess/Concrete/EFCore/EFCoreCategoryDAL.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using ShopApp.DataAccess.Abstract;
+using ShopApp.Entities;
+
+namespace ShopApp.DataAccess.Concrete.EFCore
+{
+    public class EFCoreCategoryDAL : ICategoryDAL
+    {
+        public void Create(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Category> GetAll(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetOne(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
