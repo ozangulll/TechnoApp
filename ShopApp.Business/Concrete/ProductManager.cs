cb3855fe8547ed5e192ac76b07495256a556269b
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-04-06 12:17:25 +0300

    to use dependancy injection inherited from IPRODUCTDAL

---
+++ ShopApp.Business/Concrete/ProductManager.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShopApp.Business.Abstract;
+using ShopApp.DataAccess.Abstract;
+using ShopApp.DataAccess.Concrete.EFCore;
+using ShopApp.Entities;
+
+namespace ShopApp.Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDAL _ProductDAL;
+
+        public ProductManager(IProductDAL productDAL)
+        {
+            _ProductDAL = productDAL;
+        }
+
+        public void Create(Product entity)
+        {
+           _ProductDAL.Create(entity);
+        }
+
+        public void Delete(Product entity)
+        {
+           _ProductDAL.Delete(entity);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _ProductDAL.GetAll().ToList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _ProductDAL.GetById(id);
+        }
+
+        public void Update(Product entity)
+        {
+            _ProductDAL.Update(entity);
+        }
+    }
+}
