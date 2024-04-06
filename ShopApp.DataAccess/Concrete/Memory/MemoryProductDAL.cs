d532e84fd563f29f53355a4353e2e38f3a1192f6
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-04-06 13:58:33 +0300

    MemoryProductDAL created

---
+++ ShopApp.DataAccess/Concrete/Memory/MemoryProductDAL.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using ShopApp.DataAccess.Abstract;
+using ShopApp.Entities;
+
+namespace ShopApp.DataAccess.Concrete.Memory
+{
+    public class MemoryProductDAL : IProductDAL
+    {
+        public void Create(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            var products=new List<Product>(){
+                new Product() {Id=1,Name="Samsung S6",ImageUrl="1.jpg", Price=1000},
+               new Product() {Id=2,Name="Samsung S7",ImageUrl="2.jpg", Price=1000},
+                new Product() {Id=3,Name="Samsung S8",ImageUrl="3.jpg", Price=1000}
+            };
+            return products;
+        
+        
+        }
+
+        public Product GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetOne(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetPopularProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
