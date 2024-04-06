e26f29ec77998b1d491f48ad77bf6e36ac26dd7d
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-04-06 13:57:44 +0300

    references added

--- ShopApp.WebUI/Program.cs
+++ ShopApp.WebUI/Program.cs
@@ -1,6 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ShopApp.Business.Abstract;
+using ShopApp.Business.Concrete;
+using ShopApp.DataAccess.Abstract;
+using ShopApp.DataAccess.Concrete.Memory;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+// Services configuration
+builder.Services.AddScoped<IProductDAL, MemoryProductDAL>();
+builder.Services.AddScoped<IProductService,ProductManager>();
+builder.Services.AddMvc();
+
+var app = builder.Build();
+app.MapDefaultControllerRoute();
+// HTTP request pipeline configuration
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
