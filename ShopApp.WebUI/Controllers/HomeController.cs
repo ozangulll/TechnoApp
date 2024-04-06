f913b432cfdd57e3ff28ec000dd890e682cfd3c7
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-04-06 13:58:17 +0300

    first controller and trial index added

---
+++ ShopApp.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ShopApp.Business.Abstract;
+
+namespace ShopApp.WebUI.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private IProductService _productService;
+
+        public HomeController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IActionResult Index(){
+         
+            return View(_productService.GetAll());
+       }
+    }
+}
