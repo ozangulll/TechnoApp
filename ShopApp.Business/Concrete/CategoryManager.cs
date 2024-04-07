22bd0aa5ff1f09b5148e5655eb4ca70025b77056
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-04-07 14:18:18 +0300

    to reduce error category manager created

---
+++ ShopApp.Business/Concrete/CategoryManager.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShopApp.Business.Abstract;
+using ShopApp.Entities;
+
+namespace ShopApp.Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        public void Create(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
