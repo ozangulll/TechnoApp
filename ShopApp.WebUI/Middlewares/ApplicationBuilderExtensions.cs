601164fa69ecbf1166143639a3073b1ae53eaed3
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-04-09 02:38:43 +0300

    for usability, reinstall some files/folders

--- ShopApp.WebUI/Middlewares/ApplicationBuilderExtensions.cs
+++ ShopApp.WebUI/Middlewares/ApplicationBuilderExtensions.cs
@@ -1,18 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.FileProviders;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Extensions.FileProviders;
 
 namespace ShopApp.WebUI.Middlewares
 {
     public static class ApplicationBuilderExtensions
     {
-        public static IApplicationBuilder CustomStaticFiles(this IApplicationBuilder app){
-            var path=Path.Combine(Directory.GetCurrentDirectory(),"node_modules");
-            var options= new StaticFileOptions{
-                FileProvider=new PhysicalFileProvider(path),
-                RequestPath="/modules"
+        public static IApplicationBuilder CustomStaticFiles(this IApplicationBuilder app)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "node_modules");
+
+            var options = new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(path),
+                RequestPath = "/modules"
             };
             app.UseStaticFiles(options);
             return app;
