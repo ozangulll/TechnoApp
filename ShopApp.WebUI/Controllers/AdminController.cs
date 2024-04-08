b311e094ae784a8cdd32db8e6ea6369b94162795
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-04-09 02:38:21 +0300

    for usability made restart, reinstall some things

--- ShopApp.WebUI/Controllers/AdminController.cs
+++ ShopApp.WebUI/Controllers/AdminController.cs
@@ -1,12 +1,16 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ShopApp.WebUI.Controllers
 {
-    public class AdminController
+    public class AdminController : Controller
     {
-        
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
--- ShopApp.WebUI/Controllers/HomeController.cs
+++ ShopApp.WebUI/Controllers/HomeController.cs
@@ -1,32 +1,25 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using ShopApp.Business.Abstract;
 using ShopApp.WebUI.Models;
 
 namespace ShopApp.WebUI.Controllers
 {
-    
     public class HomeController : Controller
     {
         private IProductService _productService;
-        private ICategoryService _categoryService;
-
-        public HomeController(IProductService productService, ICategoryService categoryService)
+        public HomeController(IProductService productService)
         {
             _productService = productService;
-            _categoryService = categoryService;
         }
-
-        public IActionResult Index(){
-         
-            return View(new ProductListModel(){
-                Products=_productService.GetAll()
+        public IActionResult Index()
+        {
+            return View(new ProductListModel() {
+                Products = _productService.GetPopularProduct()
             });
-       }
+        }
     }
 }
