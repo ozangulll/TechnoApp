b90a81dbfaf39b911b61974e9f1a854879c410f3
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-04-09 15:59:12 +0300

    add productcategories to the product entity

--- ShopApp.Entities/Product.cs
+++ ShopApp.Entities/Product.cs
@@ -8,8 +8,10 @@ namespace ShopApp.Entities
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string ImageUrl { get; set; }
+        public string ?Name { get; set; }
+        public string ?ImageUrl { get; set; }
+        public string Description { get; set; }
         public decimal Price { get; set; }
+        public List<ProductCategory> ProductCategories { get; set; }
     }
 }
