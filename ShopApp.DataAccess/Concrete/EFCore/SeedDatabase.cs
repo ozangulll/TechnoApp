77e9986685c86f41d89ee637540398fb0d0cfd76
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-04-09 15:58:57 +0300

    Seed data changes added

--- ShopApp.DataAccess/Concrete/EFCore/SeedDatabase.cs
+++ ShopApp.DataAccess/Concrete/EFCore/SeedDatabase.cs
@@ -18,6 +18,7 @@ namespace ShopApp.DataAccess.Concrete.EFCore
                 }
                 if(context.Products.Count()==0){
                     context.Products.AddRange(Products);
+                    context.AddRange(ProductCategory);
                 }
             }
             context.SaveChanges();
@@ -25,15 +26,23 @@ namespace ShopApp.DataAccess.Concrete.EFCore
         private static Category[] Categories =  {
             new Category(){Name="Telefon"},
             new Category(){Name="Bilgisayar"},
+             new Category(){Name="Elektronik"},
             
         };
         private static Product[] Products =  {
-            new Product(){Name="Samsung S5",Price=2000,ImageUrl="1.jpg"},
-            new Product(){Name="Samsung S6",Price=3000,ImageUrl="1.jpg"},
-            new Product(){Name="Samsung S7",Price=4000,ImageUrl="1.jpg"},
-            new Product(){Name="Samsung S8",Price=5000,ImageUrl="1.jpg"},
+            new Product(){Name="Samsung S5",Price=2000,ImageUrl="1.jpg",Description="<p> Güzel telefon</p>"},
+            new Product(){Name="Samsung S6",Price=3000,ImageUrl="2.jpg",Description="<p> Güzel telefon</p>"},
+            new Product(){Name="Samsung S7",Price=4000,ImageUrl="3.jpg",Description="<p> Güzel telefon</p>"},
+            new Product(){Name="Samsung S8",Price=5000,ImageUrl="4.jpg",Description="<p> Güzel telefon</p>"},
 
-            
+        };
+        private static ProductCategory [] ProductCategory={
+            new ProductCategory () {Product=Products[0],Category=Categories[0]},
+            new ProductCategory () {Product=Products[0],Category=Categories[2]},
+            new ProductCategory () {Product=Products[1],Category=Categories[0]},
+            new ProductCategory () {Product=Products[1],Category=Categories[3]},
+            new ProductCategory () {Product=Products[2],Category=Categories[1]},
+
         };
     }
 }
