e71c6c97c7157aa6f9b32a681487ffb6d4901f40
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-04-12 23:26:37 +0300

    add product / admin product has been added to the admin controller and has been added some front-end animations and form

--- ShopApp.WebUI/Controllers/AdminController.cs
+++ ShopApp.WebUI/Controllers/AdminController.cs
@@ -3,14 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using ShopApp.Business.Abstract;
+using ShopApp.Entities;
+using ShopApp.WebUI.Models;
 
 namespace ShopApp.WebUI.Controllers
 {
     public class AdminController : Controller
     {
+        private IProductService _productService;
+
+        public AdminController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+
         public IActionResult Index()
         {
+       
+            return View(new ProductListModel(){
+                Products=_productService.GetAll()
+            });
+        }
+        [HttpGet]
+        public IActionResult CreateProduct(){
             return View();
+        }
+        [HttpPost]
+        public IActionResult CreateProduct(ProductModel productModel){
+            var entity=new Product(){
+                Name=productModel.Name,
+                Price=productModel.Price,
+                Description=productModel.Description,
+                ImageUrl=productModel.ImageUrl
+            };
+            _productService.Create(entity);
+            return Redirect("Index");
         }
     }
 }
---
+++ ShopApp.WebUI/Models/ProductModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShopApp.Entities;
+
+namespace ShopApp.WebUI.Models
+{
+    public class ProductModel
+    {
+             public int Id { get; set; }
+        public string ?Name { get; set; }
+        public string ?ImageUrl { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public List<ProductCategory> ProductCategories { get; set; }
+    }
+}
