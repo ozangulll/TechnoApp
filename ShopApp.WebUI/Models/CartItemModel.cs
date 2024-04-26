982f591d65c5192b8e088e620b1d0e258a66e31d
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-04-26 22:54:38 +0300

    Total prica calculated

--- ShopApp.WebUI/Models/CartItemModel.cs
+++ ShopApp.WebUI/Models/CartItemModel.cs
@@ -8,6 +8,9 @@ namespace ShopApp.WebUI.Models
     public class CartModel{
         public int CartId { get; set; }
         public List<CartItemModel> CartItems { get; set; }
+        public decimal TotalPrice(){
+            return CartItems.Sum(i=>i.Price*i.Quantity);
+        }
     }
     public class CartItemModel
     {
